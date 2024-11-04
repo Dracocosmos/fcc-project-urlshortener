3cee486b428ab66d9c23ab43fb6de67469f65e6d
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-11-04 12:45:16 +0200

    added --watch to package.json run command, added route in expressjs.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const dns = require('node:dns');
+dnsPromises = dns.promises;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +20,33 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const parser = bodyParser.urlencoded({
+  extended: true
+})
+app.post('/api/shorturl', parser, function(req, res) {
+  const isValidUrl = async urlString => {
+    let url;
+    try {
+      url = new URL(urlString);
+    }
+    catch (e) {
+      return false;
+    }
+
+    const validProtocol = url.protocol === "http:"
+      || url.protocol === "https:";
+
+    //TODO: this is not synchronous, parent function will not wait for it:
+    let validHost = await dnsPromises.lookup(url.hostname)
+
+    return validProtocol && validHost
+  }
+  console.log(isValidUrl(req.body.url))
+  // console.log(req.body.url)
+  // console.log(req.body)
+  res.send(req.body)
 });
 
 app.listen(port, function() {
